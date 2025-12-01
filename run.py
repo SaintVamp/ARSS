7c13646973294ab08194a411d64ecc99e4abe6ef
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-01 15:00:00 +0000

    修复CS和ZM自由域名POSTER问题

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-11-28 14:13:00"
-ts = 1764310380
+ver = "2025-12-01 22:59:59"
+ts = 1764601199
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
