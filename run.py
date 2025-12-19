8facdb849b7c8b8339c0e63ebad7cf8356539e7c
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-19 06:03:21 +0000

    1、增加馒头源站 2、增加配置页面 3、容器环境变量需要加密码

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-19 12:41:26"
-ts = 1766119286
+ver = "2025-12-19 14:03:20"
+ts = 1766124200
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
