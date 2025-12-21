98105546f4f4d8149c65f491201268173b8e36cc
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-21 01:21:29 +0000

    馒头识别BUG修复

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-20 22:22:13"
-ts = 1766240533
+ver = "2025-12-21 09:21:27"
+ts = 1766280087
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
