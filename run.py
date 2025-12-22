2b832ff4a07d9b86ee76dddcd2f160b2229f8f8f
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-22 13:57:41 +0000

    观众动画类型修复

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-22 14:36:33"
-ts = 1766385393
+ver = "2025-12-22 21:57:40"
+ts = 1766411860
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
