079f547505ec88cb608f14db4af087212a2c6bb1
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-01-04 23:16:02 +0000

    增加CookieCloud功能

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-01-04 17:27:25"
-ts = 1767518845
+ver = "2026-01-05 07:16:01"
+ts = 1767568561
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
