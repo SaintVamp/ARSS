b3468e4277a3e71f84b69102cb2aa85c99ce803d
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-01-15 13:35:35 +0000

    增加修改配置后自动重启 配置页面增加【发布时间限制】

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-01-09 18:05:21"
-ts = 1767953121
+ver = "2026-01-15 21:35:33"
+ts = 1768484133
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
